dcf5e51390dad317aecfd083d35f32f3a6a43674
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2025-01-06 23:29:51 +0100

    [UPD] added variables for the player stats to script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+// Player stats
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeaponIndex = 0;
+let inventory = ["stick"]
+
+// Monster stats
+let monsterHealth
+
+//fight
+let fighting

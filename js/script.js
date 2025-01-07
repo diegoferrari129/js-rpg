e13d069cd8502301def57c5534bdf7f5032b9832
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2025-01-07 23:42:41 +0100

    [UPD] added variables for text, xpText, healthText, goldText, monsterStats, and monsterName with querySelector to script.js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@ let gold = 50;
 let currentWeaponIndex = 0;
 let inventory = ["stick"]
 
+const xpText = document.querySelector('#xpText');
+const healthText = document.querySelector('#healthText');
+const goldText = document.querySelector('#goldText');
+
 //controls
 const button1 = document.querySelector("#button1")
 const button2 = document.querySelector("#button2")
@@ -12,6 +16,11 @@ const button3 = document.querySelector("#button3")
 
 // Monster stats
 let monsterHealth
+const monsterStats = document.querySelector('#monsterStats');
+const monsterName = document.querySelector('#monsterName');
+
+//text
+const text = document.querySelector('#text');
 
 //fight
 let fighting

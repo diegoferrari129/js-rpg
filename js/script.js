6f9da40561df80dbe41450a94a8c9f77bd41e956
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2025-01-08 23:09:06 +0100

    [UPD] updating goStore function

--- js/script.js
+++ js/script.js
@@ -15,7 +15,13 @@ const button2 = document.querySelector("#button2")
 const button3 = document.querySelector("#button3")
 
 function goStore() {
-    console.log("Going to store.");
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
+    text.innerText = "You enter the store.";
 }
 
 function goCave() {

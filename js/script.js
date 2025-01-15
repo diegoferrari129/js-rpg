1fb6caa743af84ab8c7fe3d632818288206e66f0
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2025-01-15 22:52:56 +0100

    [UPD] updated "sellWeapon" function

--- js/script.js
+++ js/script.js
@@ -60,6 +60,19 @@ function buyWeapon() {
     }
 }
 
+function sellWeapon(){ 
+    if (inventory.length > 1){
+        gold += 15;
+        goldText.innerText = gold;
+        let currentWeapon = inventory.shift();
+        text.innerText = "You sold a " + currentWeapon + ".";
+        text.innerText += " In your inventory you have: " + inventory + ".";
+    } else {
+        text.innerText = "Don't sell your only weapon!";
+    
+    }
+}
+
 // Monster stats
 let monsterHealth
 const monsterHealthText = document.querySelector("#monsterHealth");

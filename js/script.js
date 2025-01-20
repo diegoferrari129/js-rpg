88602ddef766cfd4e8389c9fdc83a71e8d2c5fc5
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2025-01-20 22:48:28 +0100

    [UPD] udpate "attack" function

--- js/script.js
+++ js/script.js
@@ -176,15 +176,19 @@ function goFight() {
 
 function attack() {
     text.innerText = "The " + monsters[fighting].name + " attacks.";
-    text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + "."
+    text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
     health -= monsters[fighting].level;
     monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
     healthText.innerText = health;
     monsterHealthText.innerText = monsterHealth;
-    if ( health <= 0) {
+    if (health <= 0) {
         lose();
     } else if (monsterHealth <= 0) {
-        defeatMonster();
+        if (fighting===2) {
+            winGame();
+        } else {
+            defeatMonster();
+        }
     }
 }
 function dodge() {

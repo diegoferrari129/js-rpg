43a77c986d59deae7760b9b06908697e62d58be5
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2025-01-20 23:09:07 +0100

    [UPD] updated "attack" fuunction and adding new function called "getMonsterAttackValue"

--- js/script.js
+++ js/script.js
@@ -183,7 +183,7 @@ function goFight() {
 function attack() {
     text.innerText = "The " + monsters[fighting].name + " attacks.";
     text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
-    health -= monsters[fighting].level;
+    health -= getMonsterAttackValue(monsters[fighting].level);
     monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
     healthText.innerText = health;
     monsterHealthText.innerText = monsterHealth;
@@ -197,6 +197,11 @@ function attack() {
         }
     }
 }
+
+function getMonsterAttackValue(level){
+
+}
+
 function dodge() {
     text.innerText = "You dodge the attack from the " + monsters[fighting].name + " .";
 }
